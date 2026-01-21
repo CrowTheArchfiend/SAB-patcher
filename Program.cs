13ed3a83f84ec2a66716f99e467cc0cf4e93595f
Crow <59369505+CrowTheArchfiend@users.noreply.github.com>
2026-01-21 18:13:22 +0100

    Fix errors when DLL in use
    Added a RestartExplorer that does what it sounds like to help DLL editing.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
 using System.Security.Principal;
 
 // cfg
@@ -36,12 +37,11 @@ string? foundPath = searchPaths.Select(p => Path.Combine(p, targetDll)).FirstOrD
 // exec
 if (foundPath == null)
 {
-    Console.WriteLine($"{r}Error:{c} Target DLL not found in any search directories.");
+    Console.WriteLine($"{r}Error:{c} Target DLL not found.");
 }
 else
 {
-    if (isRestore) DllRestore(foundPath);
-    else DllPatch(foundPath);
+    if (isRestore) RestartExplorer(() => DllRestore(foundPath)); else RestartExplorer(() => DllPatch(foundPath));
 }
 
 WaitForExit();
@@ -93,7 +93,20 @@ void DllRestore(string filePath)
     {
         File.Copy(backupPath, filePath, overwrite: true);
         Console.WriteLine($"{g}Success:{c} Original DLL restored from backup.");
-    } catch (Exception ex) { Console.WriteLine($"{r}Restore failure: {ex.Message}{c}"); }
+    }
+    catch (Exception ex) { Console.WriteLine($"{r}Restore failure: {ex.Message}{c}"); }
+}
+
+void RestartExplorer(Action DllAction)
+{
+    Console.WriteLine($"{y}[!] Restarting Explorer to unlock DLL...{c}");
+    foreach (var proc in Process.GetProcessesByName("explorer"))
+    {
+        try { proc.Kill(); proc.WaitForExit(); } catch { }
+    }
+    DllAction();
+    Console.WriteLine($"{g}[+] Restarting Explorer...{c}");
+    Process.Start("explorer.exe");
 }
 
 void WaitForExit()
